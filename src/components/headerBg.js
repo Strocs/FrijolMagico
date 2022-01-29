92a16e33942beb8f445d6dd26080dfba4213fa70
Strocs <iganmolina@gmail.com>
2022-01-29 18:50:40 -0300

    v290122: Cambios Página Ilustradorxs

--- src/components/headerBg.js
+++ src/components/headerBg.js
@@ -1,10 +1,22 @@
 const headerBg = () => {
-  window.onscroll = function(){
-		let top = window.scrollY;
-    let header = document.getElementsByClassName('header__container')[0]
+	window.onscroll = function(){
+	  	let top = window.scrollY;
+    	let header = document.getElementsByClassName('header__container')[0]
+		let headerSize = document.getElementsByClassName('header__main')[0]
+		let logoText = document.getElementsByClassName('logo__text')[0]
 
-		top > 0 ? header.classList.add('header__container-bg') : header.classList.remove('header__container-bg');
+		top > 0 ? 	(header.classList.add('header__container-bg'), 
+					headerSize.classList.add('header__main-bg'),
+					logoText.classList.add('logo__text-bg')
+					) 
+					: 
+					(header.classList.remove('header__container-bg'),
+					headerSize.classList.remove('header__main-bg'),
+					logoText.classList.remove('logo__text-bg')
+					);
+
 	};
+
 }
 
 export default headerBg
