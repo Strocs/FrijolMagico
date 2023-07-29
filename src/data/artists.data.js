28a86b9dc925a281286c88fb166adc31015c1d3e
Strocs <strocsdev@gmail.com>
2023-07-29 16:02:25 -0400

    improve artist images data

--- src/data/artists.data.js
+++ src/data/artists.data.js
@@ -6,7 +6,12 @@ export const artistsData = [
 			ig: 'glitter.illustration',
 			mail: 'List.retamal@gmail.com'
 		},
-		images: ['a1', 'a2', 'a3', 'a4'],
+		images: [
+			{ url: 'a1.png', size: { width: 1080, height: 1080, aspectRatio: '1 / 1' } },
+			{ url: 'a2.png', size: { width: 1080, height: 1080, aspectRatio: '1 / 1' } },
+			{ url: 'a3.png', size: { width: 1080, height: 1080, aspectRatio: '1 / 1' } },
+			{ url: 'a4.png', size: { width: 764, height: 1080, aspectRatio: '191 / 270' } }
+		],
 		location: 'IV Región, Coquimbo',
 		bio: 'Mi trabajo consiste principalmente en realizar ilustraciones digitales, las temáticas que abordo son el misticismo, feminidad, veganismo y la psicodelia, estas ilustraciones posteriormente las incorporo en diversos productos y papelería.'
 	},
@@ -21,7 +26,12 @@ export const artistsData = [
 			twitter: 'bluestraycatart',
 			mail: 'gabriela95_contreras@hotmail.com'
 		},
-		images: ['b1', 'b2', 'b3', 'b4'],
+		images: [
+			{ url: 'b1.png', size: { width: 1080, height: 1080, aspectRatio: '1 / 1' } },
+			{ url: 'b2.png', size: { width: 1080, height: 1080, aspectRatio: '1 / 1' } },
+			{ url: 'b3.png', size: { width: 1080, height: 1080, aspectRatio: '1 / 1' } },
+			{ url: 'b4.png', size: { width: 1080, height: 1080, aspectRatio: '1 / 1' } }
+		],
 		location: 'IV Región, Coquimbo',
 		bio: 'Me gusta mucho basarme en el género de fantasía oscura, donde puedo buscar muchas posibilidades para explorar conceptos e ideas con un enfoque más personal, a través de tinta y achurado, busco valorizar luces, sombras y detalles al máximo.'
 	},
@@ -35,7 +45,12 @@ export const artistsData = [
 			youtube: 'channel/UCrjJGDr9qHlWMdGDmRlvMlw',
 			mail: 'camihlatournerie@gmail.com'
 		},
-		images: ['c1', 'c2', 'c3', 'c4'],
+		images: [
+			{ url: 'c1.png', size: { width: 1080, height: 1080, aspectRatio: '1 / 1' } },
+			{ url: 'c2.png', size: { width: 786, height: 1080, aspectRatio: '131 / 180' } },
+			{ url: 'c3.png', size: { width: 743, height: 1080, aspectRatio: '743 / 1080' } },
+			{ url: 'c4.png', size: { width: 985, height: 1080, aspectRatio: '197 / 216' } }
+		],
 		location: 'IV Región, Coquimbo',
 		bio: 'Artista e ilustradora dedicada principalmente al trabajo con medios tradicionales como la acuarela. <br><br>Se especializa en ilustraciones semi-realistas con temáticas delicadas y femeninas de  tono nostálgico, cálido y emocional. <br><br>Su trabajo toma inspiración principalmente de los cuentos de hadas, la literatura clásica, la magia, la naturaleza, las emociones humanas  y  las diferentes culturas del mundo,  siempre desde un ángulo distintivamente emotivo, femenino y lleno de realismo mágico.'
 	},
@@ -50,7 +65,11 @@ export const artistsData = [
 			web: 'https://camipepe.com/',
 			mail: 'camiiipepe@gmail.com'
 		},
-		images: ['d1', 'd2', 'd3'],
+		images: [
+			{ url: 'd1.png', size: { width: 1080, height: 1080, aspectRatio: '1 / 1' } },
+			{ url: 'd2.png', size: { width: 1080, height: 1080, aspectRatio: '1 / 1' } },
+			{ url: 'd3.png', size: { width: 1080, height: 1080, aspectRatio: '1 / 1' } }
+		],
 		location: 'IV Región, La Serena',
 		bio: 'Somos “Camipepe” Camila Olivares (Cami) y José Flores (Pepe). Nos dedicamos a la ilustración de forma independiente. Trabajamos juntos de hace 5 años, luego de salir de la carrera de Diseño Gráfico. <br><br> En nuestro trabajo nos gusta usar colores vivos y formas simples, usamos una técnica digital que nos permite tener ilustraciones versátiles. Nuestras temáticas favorita son los animales, la cultura, la naturaleza y las cosas cotidianas. <br><br>Tenemos dos lineas de trabajo: proyectos personales, donde aplicamos nuestras ilustraciones a distintos productos y Las ilustraciones que realizamos a pedido. También nos gusta enseñar y regularmente impartimos talleres de ilustración para niños y jóvenes.'
 	},
@@ -62,7 +81,11 @@ export const artistsData = [
 			ig: 'c_a_t_a_n_a',
 			mail: 'a.gutierrezuribe@gmail.com'
 		},
-		images: ['e1', 'e2', 'e3'],
+			images: [
+			{ url: 'e1.png', size: { width: 1080, height: 1080, aspectRatio: '1 / 1' } },
+			{ url: 'e2.png', size: { width: 1080, height: 1080, aspectRatio: '1 / 1' } },
+			{ url: 'e3.png', size: { width: 1080, height: 1080, aspectRatio: '1 / 1' } }
+		],
 		location: 'IV Región, La Serena',
 		bio: 'Mi trabajo combina cotidianidad con el imaginario de elementos surrealistas y espaciales, inspirados en la cultura pop y/o la contingencia social.'
 	},
@@ -75,7 +98,12 @@ export const artistsData = [
 			ig: 'collarcitosrv',
 			mail: 'collarcitos@gmail.com'
 		},
-		images: ['f1', 'f2', 'f3', 'f4'],
+		images: [
+			{ url: 'f1.png', size: { width: 1080, height: 1080, aspectRatio: '1 / 1' } },
+			{ url: 'f2.png', size: { width: 1080, height: 1080, aspectRatio: '1 / 1' } },
+			{ url: 'f3.png', size: { width: 1080, height: 1080, aspectRatio: '1 / 1' } },
+			{ url: 'f4.png', size: { width: 1080, height: 1080, aspectRatio: '1 / 1' } }
+		],
 		location: 'V Región, La Serena',
 		bio: 'Totalmente tradicional: acuarela, marcadores, tinta china, tiralíneas, entre estos materiales me muevo. <br><br>Todo muy orgánico, animales, vegetación y algo de cultura popular es lo que pueden ver en mis trabajos, y por supuesto que también algún guiño a la contingencia.'
 	},
@@ -88,7 +116,11 @@ export const artistsData = [
 			ig: 'crowfin_art',
 			mail: 'cristian.p.marin@gmail.com'
 		},
-		images: ['g1', 'g2', 'g3'],
+		images: [
+			{ url: 'g1.png', size: { width: 755, height: 1080, aspectRatio: '151 / 216' } },
+			{ url: 'g2.png', size: { width: 784, height: 1080, aspectRatio: '98 / 135' } },
+			{ url: 'g3.png', size: { width: 784, height: 1080, aspectRatio: '98 / 135' } }
+		],
 		location: 'IV Región, Coquimbo',
 		bio: 'Mi nombre es Cristian Marin, alias “cris crowfin”, y soy un aficionado a la ilustración desde siempre. <br><br>Teniendo un estilo básico en ilustración tradicional, con clara influencia en el manga japonés, gusto de dibujar fan arts de mis personajes e historias favoritas. Aun así, mi creatividad no deja de inspirarme y muchas veces creo mis propios diseños. <br><br>Actualmente sigo en camino por mejorar mi estilo, aprendiendo técnicas digitales para incorporar a mis creaciones. Todo con el objetivo de realizar grandes proyectos, trabajar con otros grandes artistas y profesionalizar mi arte.<'
 	},
@@ -101,7 +133,12 @@ export const artistsData = [
 			ig: 'drommer_art',
 			mail: 'drommer.art@gmail.com'
 		},
-		images: ['h1', 'h2', 'h3', 'h4'],
+		images: [
+			{ url: 'h1.png', size: { width: 1528, height: 1080, aspectRatio: '191 / 135' } },
+			{ url: 'h2.png', size: { width: 895, height: 1080, aspectRatio: '179 / 216' } },
+			{ url: 'h3.png', size: { width: 997, height: 1080, aspectRatio: '997 / 1080' } },
+			{ url: 'h4.png', size: { width: 1553, height: 1080, aspectRatio: '1553 / 1080' } }
+		],
 		location: 'IV Región, La Serena',
 		bio: 'Mi trabajo se basa en su mayoría en el dibujo digital, centrándome en el realismo a la hora de ponerme a trabajar. <br /><br />Me gusta mucho experimentar con colores fuertes y con alto contraste, creando una paleta única en cada una de mis obras. Me encanta la fantasía y el folclore japonés, por lo que estos elementos los utilizo mucho cuando comienzo a dibujar y es algo que podrás encontrar fácilmente en mi galería.'
 	},
@@ -114,7 +151,12 @@ export const artistsData = [
 			ig: 'e.vincent.v',
 			mail: 'e.vincentdiseno@gmail.com'
 		},
-		images: ['i1', 'i2', 'i3', 'i4'],
+		images: [
+			{ url: 'i1.png', size: { width: 1080, height: 1080, aspectRatio: '1 / 1' } },
+			{ url: 'i2.png', size: { width: 1020, height: 1080, aspectRatio: '17 / 18' } },
+			{ url: 'i3.png', size: { width: 759, height: 1080, aspectRatio: '253 / 360' } },
+			{ url: 'i4.png', size: { width: 764, height: 1080, aspectRatio: '191 / 270' } }
+		],
 		location: 'IV Región, Coquimbo',
 		bio: 'Me dedico a experimentar con diversas ilustraciones y cómics tratando de darle una vuelta creativa a las cosas que me rodean, mediante esto puedo encontrar nuevas maneras de conocer y entender, a veces se logra otras no tanto...'
 	},
@@ -129,7 +171,11 @@ export const artistsData = [
 			tiktok: '@eline1three',
 			mail: 'eline1three@gmail.com'
 		},
-		images: ['j1', 'j2', 'j3'],
+		images: [
+			{ url: 'j1.png', size: { width: 1174, height: 1080, aspectRatio: '587 / 540' } },
+			{ url: 'j2.png', size: { width: 1080, height: 1080, aspectRatio: '1 / 1' } },
+			{ url: 'j3.png', size: { width: 1080, height: 1080, aspectRatio: '1 / 1' } }
+		],
 		location: 'IV Región, Coquimbo',
 		bio: 'Hello! Mi nombre es Eline y soy ilustradora digital y tradicional. Mi fuerte es el estilo anime y me gusta dibujar personajes de mis series favoritas. <br><br>También soy riggeadora de vtubers, básicamente creo personajes que puedan seguir el movimiento de la cara, muchas gracias por leer!'
 	},
@@ -142,7 +188,11 @@ export const artistsData = [
 			ig: 'elmenese',
 			mail: 'ivanjorquera.o@gmail.com'
 		},
-		images: ['k1', 'k2', 'k3'],
+		images: [
+			{ url: 'k1.png', size: { width: 1080, height: 1080, aspectRatio: '1 / 1' } },
+			{ url: 'k2.png', size: { width: 1080, height: 1080, aspectRatio: '1 / 1' } },
+			{ url: 'k3.png', size: { width: 1080, height: 1080, aspectRatio: '1 / 1' } }
+		],
 		location: 'IV Región, La Serena',
 		bio: 'Gran Parte de mi trabajo está enfocado en el Arte urbano, ya que me dedico a realizar graffiti desde los 13 años. <br><br>En la actualidad realizo proyectos de arte público, rescate del patrimonio intangible a través de murales. Soy el productor y ejecutor de 2 versiones del proyecto colores de mi barrio, creado por la gestión territorial del departamento de cultura de Coquimbo y este año se planifica la primera Ruta de Murales Patrimoniales de la Región. <br><br>Individualmente estoy trabajando en una serie de cuadros donde mezclo la naturaleza con los elementos más urbanos, que tienen estrecha relación con el graffiti, como tag, caps y latas de aerosol, creando composiciones que contrastan pero que se pueden parecer paisajes cotidianos de nuestras ciudades.'
 	},
@@ -154,7 +204,12 @@ export const artistsData = [
 			ig: 'neeh_re',
 			mail: 'rene.f.arayaramirez@gmail.com'
 		},
-		images: ['l1', 'l2', 'l3', 'l4'],
+		images: [
+			{ url: 'l1.png', size: { width: 1080, height: 1080, aspectRatio: '1 / 1' } },
+			{ url: 'l2.png', size: { width: 1080, height: 1080, aspectRatio: '1 / 1' } },
+			{ url: 'l3.png', size: { width: 1080, height: 1080, aspectRatio: '1 / 1' } },
+			{ url: 'l4.png', size: { width: 1080, height: 1080, aspectRatio: '1 / 1' } }
+		],
 		location: 'IV Región, La Serena',
 		bio: 'Autodidacta. Con un estilo por definir, pero caracterizado por colores vibrantes y la creación constante de personajes. Experimentando entre los digital y lo análogo. <br><br>Inspirado principalmente en lo surrealista, recurriendo a elementos del ambiente, como nubes, fuego y hojas, junto a elementos de diversos seres vivos y seres humano compartiendo elementos, dando paso a nuevas formas.'
 	},
@@ -166,7 +221,11 @@ export const artistsData = [
 			ig: 'algundiazyueng',
 			mail: 'diazyueng@gmail.com'
 		},
-		images: ['m1', 'm2', 'm3'],
+		images: [
+			{ url: 'm1.png', size: { width: 1080, height: 1080, aspectRatio: '1 / 1' } },
+			{ url: 'm2.png', size: { width: 1080, height: 1080, aspectRatio: '1 / 1' } },
+			{ url: 'm3.png', size: { width: 1080, height: 1080, aspectRatio: '1 / 1' } }
+		],
 		location: 'IV Región, La Serena',
 		bio: 'Mi trabajo en la parte personal está enfocado principalmente en dibujar cosas cotidianas y dudas existenciales.'
 	},
@@ -178,7 +237,11 @@ export const artistsData = [
 			ig: 'ojus___',
 			mail: 'ojuspocus1@gmail.com'
 		},
-		images: ['n1', 'n2', 'n3'],
+		images: [
+			{ url: 'n1.png', size: { width: 1080, height: 1080, aspectRatio: '1 / 1' } },
+			{ url: 'n2.png', size: { width: 1080, height: 1080, aspectRatio: '1 / 1' } },
+			{ url: 'n3.png', size: { width: 1080, height: 1080, aspectRatio: '1 / 1' } }
+		],
 		location: 'IV Región, Ovalle',
 		bio: 'Soy Licenciada en diseño y docente de artes, en mis manos siempre tengo lápices o plumones y ahora el iPad que me abrió un mundo de posibilidades que complemento con illustraitor y muchos tutoriales. <br><br>Soy amante de la vegetación y las energías, casi todo mi trabajo se basa en eso y me libera bastante. Trato de unir el diseño y la docencia constantemente y adquirir más conocimiento día a día.'
 	},
@@ -190,7 +253,11 @@ export const artistsData = [
 			ig: 'prrr.miaow',
 			mail: 'Karenvalen.diseno@gmail.com'
 		},
-		images: ['o1', 'o2', 'o3'],
+		images: [
+			{ url: 'o1.png', size: { width: 1080, height: 1080, aspectRatio: '1 / 1' } },
+			{ url: 'o2.png', size: { width: 966, height: 1080, aspectRatio: '161 / 180' } },
+			{ url: 'o3.png', size: { width: 1080, height: 1080, aspectRatio: '1 / 1' } }
+		],
 		location: 'IV Región, La Serena',
 		bio: 'Ilustraciónes principalmente de animales y también fanart, con estilo simple y kawaii, todo esto aplicado en diversos productos, como chapitas, calendarios, libretas, stickers, posters, espejos marca paginas y más, existe gran variedad de diseños y productos.'
 	},
@@ -203,7 +270,11 @@ export const artistsData = [
 			ig: 'sofi_niscus',
 			mail: 'sofiniscusilustraciones@gmail.com'
 		},
-		images: ['p1', 'p2', 'p3'],
+		images: [
+			{ url: 'p1.png', size: { width: 1080, height: 1080, aspectRatio: '1 / 1' } },
+			{ url: 'p2.png', size: { width: 1311, height: 1080, aspectRatio: '437 / 360' } },
+			{ url: 'p3.png', size: { width: 1080, height: 1080, aspectRatio: '1 / 1' } }
+		],
 		location: 'IV Región, La Serena',
 		bio: 'Mi nombre es Sofía Marambio, soy estudiante universitaria e ilustradora aficionada. Mis ilustraciones son generalmente personas del sexo femenino, ya que me gusta plasmar la verdadera belleza que nace desde lo imperfecto, y que cada rasgo que te hace diferente a los demás te hace única. <br /><br />Además también me gusta ilustrar mascotas y animalitos en general, siento que en cada retrato puedo plasmar la pureza y amor que demuestras estos seres lo cual me hace muy feliz. <br /><br />Actualmente estoy experimentando con nuevas técnicas y mejorando las que ya dominaba, espero en un futuro mi trabajo llegue a muchas personas y al observarlo puedan sentir lo que hay detrás de cada una de mis obras.'
 	},
@@ -219,7 +290,11 @@ export const artistsData = [
 			web: 'https://www.sunmorales.com/',
 			mail: 'sunmorales35@gmail.com'
 		},
-		images: ['q1', 'q2', 'q3'],
+		images: [
+			{ url: 'q1.png', size: { width: 1487, height: 1080, aspectRatio: '1487 / 1080' } },
+			{ url: 'q2.png', size: { width: 1080, height: 1080, aspectRatio: '1 / 1' } },
+			{ url: 'q3.png', size: { width: 1528, height: 1080, aspectRatio: '191 / 135' } }
+		],
 		location: 'IV Región, La Serena',
 		bio: 'Ilustradora Profesional, fotógrafa, audiovisual, entusiasta de los gatos y los comics.<br /><br /> Mi forma favorita de comunicación es la imagen. Desde que tengo memoria me ha gustado contar historias y es a través del comic es que he encontrado mi medio para hacerlo, conociendo en internet una comunidad de humanos que aman los gatos tanto como yo. <br /><br />He trabajado en diferentes proyectos y mi enfoque esta en redes sociales, donde comparto mayoritariamente mis comics y videos. También tengo una línea de ilustraciones, generalmente de corte fantástico, con animales gigantes, seres antropomorfos o gatitos flotando en el espacio. Entre mis temas más comunes puedes encontrar la cotidianidad, los gatos, salud mental y la vida en general. Espero que te guste mi trabajo!'
 	},
@@ -232,7 +307,11 @@ export const artistsData = [
 			ig: 'tigre_maltes',
 			mail: 'alonsomartinez07@gmail.com'
 		},
-		images: ['r1', 'r2', 'r3'],
+		images: [
+			{ url: 'r1.png', size: { width: 1080, height: 1080, aspectRatio: '1 / 1' } },
+			{ url: 'r2.png', size: { width: 1080, height: 1080, aspectRatio: '1 / 1' } },
+			{ url: 'r3.png', size: { width: 864, height: 1080, aspectRatio: '4 / 5' } }
+		],
 		location: 'IV Región, Coquimbo',
 		bio: 'Desarrollo del arte gráfico, inspirado en la estética del cartelismo y los cómics, para generar una narrativa visual propia.'
 	}
