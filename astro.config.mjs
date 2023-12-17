d79af53663264fc13e366ffb5ee692fa4da62112
Ignacio A. Molina <strocsdev@gmail.com>
2023-12-17 20:22:08 -0300

    fix: redirects

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config'
 // https://astro.build/config
 export default defineConfig({
   redirects: {
-    '/festival': '/festival/dia-1'
+    '/festival': '/festival/dia-2'
   }
 })
