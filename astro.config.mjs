b8a0b9a87407dd01ece69964272ef116f7367571
Ignacio A. Molina <strocsdev@gmail.com>
2023-12-17 20:21:08 -0300

    fix: responsive on live page

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config'
 // https://astro.build/config
 export default defineConfig({
   redirects: {
-    '/festival': '/festival/dia-2'
+    '/festival': '/festival/dia-1'
   }
 })
