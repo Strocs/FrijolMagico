22f641080f9eadbb2f00c65972e2de98913cc43a
Ignacio A. Molina <strocsdev@gmail.com>
2023-12-17 18:32:34 -0300

    add: 2021 live page

--- src/data/LIVE_INFO.js
+++ src/data/LIVE_INFO.js
@@ -1,5 +1,5 @@
 export const LIVE_INFO = [
-  { id: '1', day: 'Viernes 16', video: 'K0wNji7fWt8' },
-  { id: '2', day: 'Sábado 17', video: '9IrEYZS9PUo' },
-  { id: '3', day: 'Domingo 18', video: 'WKRSuM85YEQ' }
+  { id: 'dia-1', day: 'Viernes 16', video: 'K0wNji7fWt8' },
+  { id: 'dia-2', day: 'Sábado 17', video: '9IrEYZS9PUo' },
+  { id: 'dia-3', day: 'Domingo 18', video: 'WKRSuM85YEQ' }
 ]
