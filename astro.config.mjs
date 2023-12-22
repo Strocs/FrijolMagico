0251e4c20202f458359f6ba55a6581fb7bbc5c5d
Ignacio A. Molina <strocsdev@gmail.com>
2023-12-22 10:00:54 -0300

    add: sharp for images optimization

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
-
 import tailwind from "@astrojs/tailwind";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://elfrijolmagico.cl',
-  integrations: [sitemap(), tailwind()]
+  integrations: [sitemap(), tailwind()],
+  output: "server",
+  adapter: vercel()
 });
