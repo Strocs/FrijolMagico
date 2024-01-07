3285fcd6032552b75b9e2c8e25db59e99706ff01
Ignacio A. Molina <strocsdev@gmail.com>
2024-01-07 12:05:11 -0300

    add: og on festival page

--- src/data/SITE_INFO.ts
+++ src/data/SITE_INFO.ts
@@ -3,5 +3,6 @@ export const SITE_INFO = {
   description:
     'Frijol Mágico es un espacio que reúne a las y los Ilustradores de la Región de Coquimbo, generando distintas instancias que ayuden a potenciar su trabajo.',
   logo: '/logo.png',
+  OGImage: '/OG.png',
   footer: 'Asociación Cultural Frijol Mágico 2024'
 }
