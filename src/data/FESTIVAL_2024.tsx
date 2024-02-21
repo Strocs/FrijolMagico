173789016b056d4941568845dfd8dfa21bd11741
Ignacio A. Molina <strocsdev@gmail.com>
2024-02-21 12:17:33 -0300

    Fix illustrators list

--- src/data/FESTIVAL_2024.tsx
+++ src/data/FESTIVAL_2024.tsx
@@ -2,225 +2,225 @@ export const PARTICIPANTS = {
   Ilustración: [
     {
       nick: 'Abejas Negras',
-      link: 'https://www.instagram.com/abejasnegras.tt/'
+      link: 'https://www.instagram.com/abejasnegras.tt/',
     },
     {
       nick: 'Adolfo Pintor',
-      link: 'https://www.instagram.com/adolfo.pintor'
+      link: 'https://www.instagram.com/adolfo.pintor',
     },
     {
       nick: 'Alvaro Rodrigo',
-      link: 'https://www.instagram.com/alvarorodrigostudio?igsh=MXdhNjA1Z3oxYm9nYQ=='
+      link: 'https://www.instagram.com/alvarorodrigostudio?igsh=MXdhNjA1Z3oxYm9nYQ==',
     },
     {
       nick: 'Anima Red',
-      link: 'https://www.instagram.com/anima.red  '
+      link: 'https://www.instagram.com/anima.red  ',
     },
     {
       nick: 'Addless7u7',
-      link: 'https://www.instagram.com/addless7u7'
+      link: 'https://www.instagram.com/addless7u7',
     },
     {
       nick: 'Blue Stray Catt',
-      link: 'https://www.instagram.com/blue_straycatt_art/?hl=es'
+      link: 'https://www.instagram.com/blue_straycatt_art/?hl=es',
     },
     {
       nick: 'Camellia Liz',
-      link: 'https://www.instagram.com/camellia.liz'
+      link: 'https://www.instagram.com/camellia.liz',
     },
     {
       nick: 'Camila Guamán',
-      link: 'https://www.instagram.com/camilaguaman.ilustracion'
+      link: 'https://www.instagram.com/camilaguaman.ilustracion',
     },
     {
       nick: 'Chilensisboy',
-      link: 'https://www.instagram.com/chilensisboy/'
+      link: 'https://www.instagram.com/chilensisboy/',
     },
     {
       nick: 'Cleib',
-      link: 'https://www.instagram.com/_cleib'
+      link: 'https://www.instagram.com/_cleib',
     },
     {
       nick: 'Edaliz',
-      link: 'https://www.instagram.com/edaliz.cl?igsh=MXN3MGZybnNsaTZn&utm_source=qr'
+      link: 'https://www.instagram.com/edaliz.cl?igsh=MXN3MGZybnNsaTZn&utm_source=qr',
     },
     {
       nick: 'Elale',
-      link: 'https://www.instagram.com/elale_ilustrador/'
+      link: 'https://www.instagram.com/elale_ilustrador/',
     },
     {
       nick: 'Fran Koneko',
-      link: 'https://www.instagram.com/fran.koneko?igsh=dGM4bzV3MDluZGth'
+      link: 'https://www.instagram.com/fran.koneko?igsh=dGM4bzV3MDluZGth',
     },
     {
       nick: 'Grabados Aleph',
-      link: 'https://www.instagram.com/grabados_aleph/'
+      link: 'https://www.instagram.com/grabados_aleph/',
     },
     {
       nick: 'Hanrra',
-      link: 'https://www.instagram.com/hanrra_artwork/'
+      link: 'https://www.instagram.com/hanrra_artwork/',
     },
     {
       nick: 'Iliduh',
-      link: 'https://www.instagram.com/iliduh/'
+      link: 'https://www.instagram.com/iliduh/',
     },
     {
       nick: 'Intercultural Arte',
-      link: 'https://www.instagram.com/intercultural_arte_?igsh=bG1oNW5zaGZ2bm5h'
+      link: 'https://www.instagram.com/intercultural_arte_?igsh=bG1oNW5zaGZ2bm5h',
     },
     {
       nick: 'Javivis',
-      link: 'https://www.instagram.com/javivis00?igsh=MXJsdzBweWd3MnV3bg=='
+      link: 'https://www.instagram.com/javivis00?igsh=MXJsdzBweWd3MnV3bg==',
     },
     {
-      nick: 'Karime Simon',
-      link: 'https://www.instagram.com/avinagretta/?hl=es'
+      nick: 'Bel Ilustravel',
+      link: 'https://www.instagram.com/bel.ilustravel/',
     },
     {
       nick: 'Leeneal',
-      link: 'https://www.instagram.com/leeneal__'
+      link: 'https://www.instagram.com/leeneal__',
     },
     {
-      nick: 'Luna Gatuna',
-      link: 'https://www.instagram.com/lunagatuna.art'
+      nick: 'Valeria Suarez',
+      link: 'https://www.instagram.com/osamentaseneljardin?igsh=dDVpMnFrdHg3ZXFm',
     },
     {
       nick: 'Negrotham',
-      link: 'https://www.instagram.com/negrotham/'
+      link: 'https://www.instagram.com/negrotham/',
     },
     {
       nick: 'NIño Pan',
-      link: 'https://www.instagram.com/elninopan'
+      link: 'https://www.instagram.com/elninopan',
     },
     {
       nick: 'Noezzal',
-      link: 'https://www.instagram.com/noezzal/'
+      link: 'https://www.instagram.com/noezzal/',
     },
     {
       nick: 'Pancho Valdivia',
-      link: 'https://www.instagram.com/pancho_valdivia/'
+      link: 'https://www.instagram.com/pancho_valdivia/',
     },
     {
       nick: 'Paperpupy',
-      link: 'https://www.instagram.com/paperpupy?igsh=MTh6ZXgxamU3MGxi'
+      link: 'https://www.instagram.com/paperpupy?igsh=MTh6ZXgxamU3MGxi',
     },
     {
       nick: 'Pez de Otoño',
-      link: 'https://pezdeotono.carrd.co'
+      link: 'https://pezdeotono.carrd.co',
     },
     {
       nick: 'Plepi',
-      link: 'https://www.instagram.com/__plepi/'
+      link: 'https://www.instagram.com/__plepi/',
     },
     {
       nick: 'Raices',
-      link: 'https://www.instagram.com/ra1ces/'
+      link: 'https://www.instagram.com/ra1ces/',
     },
     {
       nick: 'Rosuka64',
-      link: 'https://www.instagram.com/rosuka64/'
+      link: 'https://www.instagram.com/rosuka64/',
     },
     {
       nick: 'Sakanita',
-      link: 'https://www.instagram.com/_sakanita_/'
+      link: 'https://www.instagram.com/_sakanita_/',
     },
     {
       nick: 'Saturno',
-      link: 'https://www.instagram.com/sa_tu_rno?igsh=MW0xMHZlc3M1aW5pbw=='
+      link: 'https://www.instagram.com/sa_tu_rno?igsh=MW0xMHZlc3M1aW5pbw==',
     },
     {
       nick: 'Shobian',
-      link: 'https://www.instagram.com/shobian.art/'
+      link: 'https://www.instagram.com/shobian.art/',
     },
     {
       nick: 'Javo_Siniestro',
-      link: 'https://www.instagram.com/javo_siniestro/'
+      link: 'https://www.instagram.com/javo_siniestro/',
     },
     {
       nick: 'Tierramarga',
-      link: 'https://www.instagram.com/_tierramarga/'
+      link: 'https://www.instagram.com/_tierramarga/',
     },
     {
       nick: 'Toraauwu',
-      link: 'https://www.Instagram.com/toraauwu/'
+      link: 'https://www.Instagram.com/toraauwu/',
     },
     {
       nick: 'ViliZ VZ',
-      link: 'https://www.instagram.com/viliz_vz/'
+      link: 'https://www.instagram.com/viliz_vz/',
     },
     {
       nick: 'Vlad Fausto A.',
-      link: 'https://www.instagram.com/vladfaustoaoficial/?hl=es'
+      link: 'https://www.instagram.com/vladfaustoaoficial/?hl=es',
     },
     {
       nick: 'Yeco',
-      link: 'https://www.Instagram.com/_yeco'
+      link: 'https://www.Instagram.com/_yeco',
     },
   ],
   Manualidades: [
-      {
-        nick: 'De Cordillera',
-        link: 'https://www.instagram.cl/decordillera'
-      },
-      {
-        nick: 'Innuerespatches',
-        link: 'https://www.instagram.com/innuerespatches'
-      },
-      {
-        nick: 'Kao',
-        link: 'https://www.instagram.com/kao.art.work/'
-      },
-      {
-        nick: 'Minamore',
-        link: 'https://Minamore.cl'
-      },
-      {
-        nick: 'Sra Tonks',
-        link: 'https://www.instagram.com/sratonks/'
-      },
+    {
+      nick: 'De Cordillera',
+      link: 'https://www.instagram.cl/decordillera',
+    },
+    {
+      nick: 'Innuerespatches',
+      link: 'https://www.instagram.com/innuerespatches',
+    },
+    {
+      nick: 'Kao',
+      link: 'https://www.instagram.com/kao.art.work/',
+    },
+    {
+      nick: 'Minamore',
+      link: 'https://Minamore.cl',
+    },
+    {
+      nick: 'Sra Tonks',
+      link: 'https://www.instagram.com/sratonks/',
+    },
   ],
-  "Narrativa-Gráfica": [
+  'Narrativa-Gráfica': [
     {
       nick: 'Carvajal Carvajal',
-      link: 'https://www.instagram.com/carvajalilustraciones/'
+      link: 'https://www.instagram.com/carvajalilustraciones/',
     },
     {
       nick: 'E Vincent',
-      link: 'https://www.instagram.com/e.vincent.v/'
+      link: 'https://www.instagram.com/e.vincent.v/',
     },
     {
       nick: 'Futuro Comics',
-      link: 'http://instagram.com/futurocomics'
+      link: 'http://instagram.com/futurocomics',
     },
     {
       nick: 'Polet Komiksu',
-      link: 'https://www.instagram.com/poletkomiksu?igsh=MXd1bHdsOTd6YWl4cg=='
+      link: 'https://www.instagram.com/poletkomiksu?igsh=MXd1bHdsOTd6YWl4cg==',
     },
     {
       nick: 'Rotten Monkey Inc',
-      link: 'https://www.instagram.com/rottenmonkey_inc/'
+      link: 'https://www.instagram.com/rottenmonkey_inc/',
     },
   ],
-  "Música": [
+  Música: [
     {
       nick: 'Niños del Cerro',
-      link: 'https://www.instagram.com/ninosdelcerro/'
+      link: 'https://www.instagram.com/ninosdelcerro/',
     },
     {
       nick: 'Oye, Elizabeth',
-      link: 'https://www.instagram.com/oyeelizabeth_/'
+      link: 'https://www.instagram.com/oyeelizabeth_/',
     },
     {
       nick: 'LILLiA',
-      link: 'https://www.instagram.com/lillia.cl/'
+      link: 'https://www.instagram.com/lillia.cl/',
     },
     {
       nick: 'Talina Pazz',
-      link: 'https://www.instagram.com/cat_talinapazz'
+      link: 'https://www.instagram.com/cat_talinapazz',
     },
     {
       nick: 'La Huerta',
-      link: 'https://www.instagram.com/lahuerta.banda'
+      link: 'https://www.instagram.com/lahuerta.banda',
     },
-  ]
+  ],
 }
