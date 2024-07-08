266d97fd28afc25a29420982cccabc60fdf90071
Strocs <strocsdev@gmail.com>
2024-07-08 00:16:46 -0400

    add: calls page

--- src/data/CALLS_INFO.ts
+++ src/data/CALLS_INFO.ts
@@ -8,19 +8,14 @@ interface Call {
 
 type CallInfo = Call[]
 
-export const CALLS_INFO: CallInfo = [
-  {
-    title: 'Ilustración',
-    description: 'Convocatoria Ilustración - Novela Gráfica - Manualidades',
-    bases: '2PACX-1vQrUB43au7sryIyhnjseksLMqsKOvDPM8EhAxrkgVlnFqPfJ--wnU83W-hx78CbKTPp9rZLG7uKCc5p',
-    basesExternal: '1MhLT9fEP7Nv5ye0Jr_aZV1mFSfnVXIz4ffBnqy_4jII',
-    form: '1FAIpQLScbf1M34Ujpbw08xAy-4mR5rA3t1X4EQql0EIxfuUmzlXrSYA'
+export const CALLS_INFO = {
+  'ilustracion': {
+    bases: 'https://docs.google.com/document/d/e/2PACX-1vSw95eCepDmXclZthTjzPpp5A4QoZ12PppMtTg3ynx_XFX6YnqsfqjbvIziN8XVIdrbOH1SCGHLuhWm/pub',
+    form: 'https://docs.google.com/forms/u/1/d/14q7uMp7SRUKfrVrJuaZR0jdDR9UQkBMxtCf7_Y8Uwt4'
   },
-  {
-    title: 'Música',
-    description: 'Convocatoria Bandas Musicales',
-    bases: '2PACX-1vQNnN-LxK-zxD_qrgTBEOvEEPK35IFkDbymB5JVryWwhuRUdUCoDvP65kDdrWiomjKT1ItTH3lbXsj0',
-    basesExternal: '1Q0zRPlKtOPpOXEfyongZY7qZ-p2fAgV7Uu_F-QJVQFY',
-    form: '1FAIpQLSfDOX5BG5n2fAr-5T14xUx1jrHZLk5W2A4i8Q8kRr8cxyZRaw'
+  'musica': {
+    bases: 'https://docs.google.com/document/d/e/2PACX-1vQNnN-LxK-zxD_qrgTBEOvEEPK35IFkDbymB5JVryWwhuRUdUCoDvP65kDdrWiomjKT1ItTH3lbXsj0/pub',
+    form: 'https://docs.google.com/forms/d/1FJJxalygoDLTg7DtDjVKVbNak9VwbT0Nwog_Il5jVpY'
   }
-]
+}
+
---
+++ src/data/SITE.ts
@@ -0,0 +1,7 @@
+export const SITE = {
+  podcast: 'https://open.spotify.com/show/6X92bkkwHr43dTP1Y3LPyg',
+  social_media: {
+    ig: 'https://www.instagram.com/festivalfrijolmagico/',
+    fb: 'https://facebook.com/festivalfrijolmagico'
+  }
+}
