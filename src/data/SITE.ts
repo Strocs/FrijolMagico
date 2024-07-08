89f9d55a204b253b0d643a84ae940d6b982a6a5e
Strocs <strocsdev@gmail.com>
2024-07-08 11:31:28 -0400

    add: description

--- src/data/SITE.ts
+++ src/data/SITE.ts
@@ -1,4 +1,6 @@
 export const SITE = {
+  description:
+    'Frijol Mágico es un espacio que reúne a las y los Ilustradores de la Región de Coquimbo, generando distintas instancias que ayuden a potenciar su trabajo.',
   podcast: 'https://open.spotify.com/show/6X92bkkwHr43dTP1Y3LPyg',
   social_media: {
     ig: 'https://www.instagram.com/festivalfrijolmagico/',
