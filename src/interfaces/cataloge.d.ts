64f2c73d07d675881bf181f9d7ca90719650a57a
Strocs <strocsdev@gmail.com>
2024-08-27 20:27:17 -0400

    Add: types for cataloge interfaces

---
+++ src/interfaces/cataloge.d.ts
@@ -0,0 +1,11 @@
+export interface CatalogeArtist {
+  avatar: string
+  name: string
+  city: string
+  work_area: string
+  bio: string
+  email: string
+  rrss: string
+}
+
+export type Cataloge = CatalogeArtist[]
