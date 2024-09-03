e9097bf70a6a5218af7b7fcaa46ad85b1b06c3b6
Strocs <strocsdev@gmail.com>
2024-09-03 13:37:15 -0400

    fix: animation of panel on cataloge page

--- src/components/CatalogePanel.tsx
+++ src/components/CatalogePanel.tsx
@@ -1,45 +1,58 @@
+import { cn } from '@/lib/utils'
 import {
   $isArtistPanelOpen,
   $selectedCataloge,
   setArtistPanelOpen,
 } from '@/store/cataloge'
-import { Button } from './ui/button'
+import { getInstagramUserTag } from '@/utils/getInstagramUserTag'
 import { useStore } from '@nanostores/react'
-import { Badge } from './ui/badge'
 import { Instagram, Mail } from 'lucide-react'
+import { Badge } from './ui/badge'
+import { Button } from './ui/button'
 
 export const CatalogePanel = () => {
   const isOpen = useStore($isArtistPanelOpen)
 
-  const { id, name, work_area, bio, rrss, avatar, city, email } =
+  const { name, work_area, bio, rrss, avatar, city, email } =
     useStore($selectedCataloge)
 
   function handleClosePanel() {
     setArtistPanelOpen(false)
   }
 
-  function handleOpenImage() {}
-
-  //TODO: fix animation
-
-  export function getInstagramUsername(instagramURL: string) {
-    return instagramURL.toLowerCase().split('.com/')
+  function handleOpenImage() {
+    //TODO: Implement zoom to avatar
   }
 
   return (
     <div
-      className={`${isOpen ? 'w-full' : 'w-0'} transition-[width] duration-500  max-w-[500px] relative ml-4 mt-[--top-bar-info-height] h-[calc(100dvh-var(--top-bar-info-height))] place-content-center`}>
+      className={cn([
+        '[--max-width:488px] ',
+        'fixed sm:relative z-50',
+        'w-full max-w-[--max-width]',
+        'transition-[margin-right,right] duration-500',
+        isOpen
+          ? 'right-0 sm:mr-4'
+          : '-right-[--max-width] sm:-mr-[--max-width]',
+      ])}>
       <aside
-        className={` ${isOpen ? 'space-y-4 opacity-1 right-4' : 'opacity-0 -right-full'} max-w-[500px] transition-[opacity,right] duration-500 p-8 bg-gradient-to-br from-secondary to-accent text-background rounded-2xl max-h-[80%] h-full fixed bottom-[10%] top-[10%]`}>
-        <header className="flex items-center gap-4">
+        className={cn([
+          'fixed bottom-0 top-0 sm:top-[10%]',
+          'w-full h-full max-w-[--max-width] sm:max-h-[84vh] space-y-4 p-8',
+          'transition-[opacity] duration-500 bg-white text-primary sm:rounded-2xl',
+          isOpen ? 'opacity-1' : '',
+        ])}>
+        <header className="flex items-center gap-x-4">
           <div
             onClick={handleOpenImage}
-            className="rounded-full w-28 aspect-square  bg-zinc-600 shrink-0"
+            className="rounded-full w-28 sm:w-36 aspect-square bg-zinc-600 shrink-0"
           />
+
           <div className="space-y-2">
             <h2 className="text-4xl font-bold">{name}</h2>
+
             <div className="space-x-2">
-              <Badge className="text-sm bg-primary hover:bg-foreground text-secondary-foreground">
+              <Badge className="text-sm bg-secondary hover:bg-foreground text-secondary-foreground">
                 {work_area}
               </Badge>
               <Badge className="text-sm bg-primary hover:bg-foreground text-secondary-foreground">
@@ -48,20 +61,31 @@ export const CatalogePanel = () => {
             </div>
           </div>
         </header>
-        <p className="font-medium">{bio}</p>
+
+        <p className="font-medium xl:text-sm 2xl:text-base">{bio}</p>
 
         <footer>
-          <h3 className="text-xl font-bold">Contacto</h3>
-          <a href="" className="inline-flex gap-x-2">
+          <h3 className="text-xl font-bold mb-2">Contacto</h3>
+          <a
+            target="_blank"
+            href={'mailto:' + email}
+            className="inline-flex gap-x-2 hover:text-secondary transition duration-150">
             <Mail className="shrink-0" /> {email.toLowerCase()}
           </a>
-          <a href="" className="inline-flex gap-x-2">
-            <Instagram className="shrink-0" /> {rrss}
+          <a
+            target="_blank"
+            href={rrss}
+            className="inline-flex gap-x-2 hover:text-secondary transition duration-150">
+            <Instagram className="shrink-0" /> {getInstagramUserTag(rrss)}
           </a>
         </footer>
+
         <Button
           size="sm"
-          className={`absolute bottom-8 left-8 bg-secondary outline outline-background  hover:bg-primary transition duration-300 text-secondary-foreground shrink-0`}
+          className={cn([
+            'absolute bottom-8 right-8 sm:left-8 sm:right-auto',
+            'bg-secondary  hover:bg-primary transition duration-300 text-secondary-foreground shrink-0',
+          ])}
           onClick={handleClosePanel}>
           Cerrar
         </Button>
