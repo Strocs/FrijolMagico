f2b2be16df90e725095ba320c2a5c9fc91b33bce
Strocs <strocsdev@gmail.com>
2024-09-03 09:52:38 -0400

    add: animation on cataloge panel

--- src/components/CatalogePanel.tsx
+++ src/components/CatalogePanel.tsx
@@ -5,6 +5,8 @@ import {
 } from '@/store/cataloge'
 import { Button } from './ui/button'
 import { useStore } from '@nanostores/react'
+import { Badge } from './ui/badge'
+import { Instagram, Mail } from 'lucide-react'
 
 export const CatalogePanel = () => {
   const isOpen = useStore($isArtistPanelOpen)
@@ -16,15 +18,54 @@ export const CatalogePanel = () => {
     setArtistPanelOpen(false)
   }
 
+  function handleOpenImage() {}
+
+  //TODO: fix animation
+
+  export function getInstagramUsername(instagramURL: string) {
+    return instagramURL.toLowerCase().split('.com/')
+  }
+
   return (
-    isOpen && (
-      <div className="w-full max-w-[400px] relative mr-4">
-        <aside className="p-4 bg-accent rounded-xl sticky top-14">
-          <h2>{name}</h2>
-          <p>{bio}</p>
-          <Button onClick={handleClosePanel}>Cerrar</Button>
-        </aside>
-      </div>
-    )
+    <div
+      className={`${isOpen ? 'w-full' : 'w-0'} transition-[width] duration-500  max-w-[500px] relative ml-4 mt-[--top-bar-info-height] h-[calc(100dvh-var(--top-bar-info-height))] place-content-center`}>
+      <aside
+        className={` ${isOpen ? 'space-y-4 opacity-1 right-4' : 'opacity-0 -right-full'} max-w-[500px] transition-[opacity,right] duration-500 p-8 bg-gradient-to-br from-secondary to-accent text-background rounded-2xl max-h-[80%] h-full fixed bottom-[10%] top-[10%]`}>
+        <header className="flex items-center gap-4">
+          <div
+            onClick={handleOpenImage}
+            className="rounded-full w-28 aspect-square  bg-zinc-600 shrink-0"
+          />
+          <div className="space-y-2">
+            <h2 className="text-4xl font-bold">{name}</h2>
+            <div className="space-x-2">
+              <Badge className="text-sm bg-primary hover:bg-foreground text-secondary-foreground">
+                {work_area}
+              </Badge>
+              <Badge className="text-sm bg-primary hover:bg-foreground text-secondary-foreground">
+                {city}
+              </Badge>
+            </div>
+          </div>
+        </header>
+        <p className="font-medium">{bio}</p>
+
+        <footer>
+          <h3 className="text-xl font-bold">Contacto</h3>
+          <a href="" className="inline-flex gap-x-2">
+            <Mail className="shrink-0" /> {email.toLowerCase()}
+          </a>
+          <a href="" className="inline-flex gap-x-2">
+            <Instagram className="shrink-0" /> {rrss}
+          </a>
+        </footer>
+        <Button
+          size="sm"
+          className={`absolute bottom-8 left-8 bg-secondary outline outline-background  hover:bg-primary transition duration-300 text-secondary-foreground shrink-0`}
+          onClick={handleClosePanel}>
+          Cerrar
+        </Button>
+      </aside>
+    </div>
   )
 }
