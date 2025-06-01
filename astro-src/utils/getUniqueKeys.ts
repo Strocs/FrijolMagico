f03c0a070a8298298199677ed898af016c9f6fa1
Strocs <strocsdev@gmail.com>
2025-06-01 17:58:36 -0400

    migration: delete old astro files

--- astro-src/utils/getUniqueKeys.ts
+++
@@ -1,7 +0,0 @@
-import type { Cataloge } from '@/interfaces/cataloge'
-
-export const getFiltersData = (data: Cataloge, key: 'city' | 'work_area') => {
-  const uniqueData = new Set(data.map((d) => d[key]))
-
-  return [...uniqueData].map((data) => ({ value: data }))
-}
--- astro-src/utils/normalizeString.ts
+++
@@ -1,9 +0,0 @@
-type NormalizeString = (text: string) => string
-
-export const normalizeString: NormalizeString = (text) => {
-  return text
-    .trim()
-    .toLowerCase()
-    .normalize('NFD')
-    .replace(/[^\w-]+/g, '')
-}
