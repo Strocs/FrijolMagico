664941126a8fddb96cb61edf0199c3717b842755
Strocs <strocsdev@gmail.com>
2025-06-26 22:04:55 -0400

    add(lib): google spreadsheet controller

---
+++ src/lib/googleSpreadsheetController.ts
@@ -0,0 +1,41 @@
+import { GoogleSpreadsheet } from 'google-spreadsheet'
+
+interface GoogleSpreadsheetConfig {
+  sheetId: string | undefined
+  apiKey: string | undefined
+  headers: Record<string, string>
+}
+
+export const googleSpreadsheetController = async <T>({
+  sheetId,
+  apiKey,
+  headers,
+}: GoogleSpreadsheetConfig): Promise<T[]> => {
+  if (!sheetId || !apiKey) {
+    console.error('Missing Google Sheets configuration')
+    return []
+  }
+
+  try {
+    const doc = new GoogleSpreadsheet(sheetId, {
+      apiKey,
+    })
+
+    await doc.loadInfo()
+    const sheet = doc.sheetsByIndex[0]
+    const rows = await sheet.getRows()
+
+    // Process the rows into T format
+    const data = rows.map(async (row) =>
+      Object.entries(headers).reduce((acc, [key, value]) => {
+        acc[key as keyof T] = row.get(value) || ''
+        return acc
+      }, {} as T),
+    )
+
+    return await Promise.all(data)
+  } catch (error) {
+    console.error('Error fetching Google Sheets data:', error)
+    return []
+  }
+}
