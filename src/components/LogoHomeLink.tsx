0e5511be8a406534d0b6beca5a8f928bf08d945f
Strocs <strocsdev@gmail.com>
2025-07-02 17:27:43 -0400

    fix: paths from centralized path files

--- src/components/LogoHomeLink.tsx
+++ src/components/LogoHomeLink.tsx
@@ -1,10 +1,11 @@
 import Image from 'next/image'
 import Link from 'next/link'
+import { paths } from '@/config/paths'
 
 export const LogoHomeLink = () => {
   return (
     <Link
-      href='/'
+      href={paths.home}
       target='_self'
       className='group relative z-50 m-auto block size-fit'>
       <span className='font-noto text-fm-orange absolute -top-2 right-0 left-0 mx-auto rotate-[32deg] text-center font-bold opacity-0 transition-opacity duration-100 group-hover:opacity-100'>
