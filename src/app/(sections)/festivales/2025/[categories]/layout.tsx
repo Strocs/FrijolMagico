7012de30fbfda67ff79e986256945e14db5ad74a
Strocs <strocsdev@gmail.com>
2025-07-02 17:29:35 -0400

    add: temp image for 2025 festival background

--- src/app/(sections)/festivales/2025/[categories]/layout.tsx
+++ src/app/(sections)/festivales/2025/[categories]/layout.tsx
@@ -1,5 +1,6 @@
 import { Header } from '@/components/Header'
 import siteData from '@/data/site.json'
+// import Image from 'next/image'
 
 const { selected_artists } = siteData
 
@@ -14,7 +15,16 @@ export default function ApprovedArtistsLayout({
         title={selected_artists.title}
         subTitle={selected_artists.subtitle}
       />
-      <main className='container mx-auto h-full px-4 py-8'>{children}</main>
+      {/* <Image
+        src='/temp.jpeg'
+        alt='Festivales 2025'
+        width={1920}
+        height={1080}
+        className='fixed -top-1/2 right-0 left-0 -z-10'
+      /> */}
+      <main className='relative container mx-auto h-full px-4 py-8'>
+        {children}
+      </main>
     </>
   )
 }
