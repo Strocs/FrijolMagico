238e1c792858ec24088dc835ab0a0422f54554f8
Strocs <strocsdev@gmail.com>
2025-07-02 18:54:24 -0400

    fix: remove .eslintignore and update eslint config to include ignored paths

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,9 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends('next/core-web-vitals', 'next/typescript', 'prettier'),
+  {
+    ignores: ['node_modules', 'dist'],
+  },
 ]
 
 export default eslintConfig
