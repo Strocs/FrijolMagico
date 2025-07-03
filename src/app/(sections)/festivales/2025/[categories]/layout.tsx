9f802b1b8c1e976e986fb523058a2776b2d4c25e
Strocs <strocsdev@gmail.com>
2025-07-03 19:11:55 -0400

    refactor: remove unused imports and commented-out code in ApprovedArtistsLayout

--- src/app/(sections)/festivales/2025/[categories]/layout.tsx
+++ src/app/(sections)/festivales/2025/[categories]/layout.tsx
@@ -1,6 +1,6 @@
+// import { Parallax } from '@/components/approved-artists/Parallax'
 import { Header } from '@/components/Header'
 import siteData from '@/data/site.json'
-// import Image from 'next/image'
 
 const { selected_artists } = siteData
 
@@ -15,13 +15,6 @@ export default function ApprovedArtistsLayout({
         title={selected_artists.title}
         subTitle={selected_artists.subtitle}
       />
-      {/* <Image
-        src='/temp.jpeg'
-        alt='Festivales 2025'
-        width={1920}
-        height={1080}
-        className='fixed -top-1/2 right-0 left-0 -z-10'
-      /> */}
       <main className='relative container mx-auto h-full px-4 py-8'>
         {children}
       </main>
