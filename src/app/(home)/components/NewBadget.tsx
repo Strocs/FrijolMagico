fe9ac862c82358c9edec4c56867e79791c378cff
Strocs <strocsdev@gmail.com>
2025-07-09 22:10:31 -0400

    style: refactor NewBadget component to accept customizable colors and improve class handling

--- src/app/(home)/components/NewBadget.tsx
+++ src/app/(home)/components/NewBadget.tsx
@@ -1,6 +1,18 @@
-export const NewBadget = () => {
+import { cn } from '@/lib/utils'
+
+export const NewBadget = ({
+  color = 'text-fm-orange',
+  backgroundColor = 'bg-fm-white',
+  outlineColor = 'outline-fm-white',
+}) => {
   return (
-    <span className='outline-fm-white bg-fm-orange absolute -top-2 -left-2 -rotate-6 rounded-md px-2 font-bold outline-2'>
+    <span
+      className={cn(
+        'absolute -top-2 -left-2 z-20 -rotate-6 rounded-md px-2 font-bold outline-2',
+        color,
+        backgroundColor,
+        outlineColor,
+      )}>
       Nuevo!
     </span>
   )
