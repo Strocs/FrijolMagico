56e8e0a80a69c29a7b16d27246ec014327219771
Strocs <strocsdev@gmail.com>
2025-07-09 23:27:49 -0400

    style: refactor Header component to utilize utility function for class names and streamline styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,23 +1,26 @@
 import ReactMarkdown from 'react-markdown'
 import { DoodleLine } from './DoodleLine'
+import { cn } from '@/lib/utils'
 
 export const Header = ({
   title,
   subTitle,
   description,
+  fontStyle = 'font-josefin',
 }: {
   title: string
   subTitle?: string
   description?: string
+  fontStyle?: string
 }) => {
   return (
-    <header className='text-fm-green space-y-4 px-2 pt-16'>
+    <header className={cn('text-fm-green space-y-4 px-2 pt-16', fontStyle)}>
       <div className='flex flex-col gap-2'>
         <ReactMarkdown
           components={{
             h1: ({ ...props }) => (
               <h1
-                className='font-josefin from-fm-orange to-fm-yellow bg-gradient-to-r bg-clip-text py-2 text-center text-5xl text-transparent sm:text-6xl'
+                className='from-fm-orange to-fm-yellow bg-gradient-to-r bg-clip-text py-2 text-center text-5xl text-transparent sm:text-6xl'
                 {...props}
               />
             ),
@@ -29,7 +32,7 @@ export const Header = ({
             components={{
               h2: ({ ...props }) => (
                 <h2
-                  className='font-josefin text-fm-orange -mt-6 py-2 text-center text-4xl'
+                  className='text-fm-orange -mt-6 py-2 text-center text-4xl'
                   {...props}
                 />
               ),
