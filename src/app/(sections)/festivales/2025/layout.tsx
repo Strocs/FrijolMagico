b647d5f8af2c3429f50abe7a5238ba1b03b56b70
Strocs <strocsdev@gmail.com>
2025-07-09 22:10:20 -0400

    style: add SuperFortress font and related styles for Festival 2025 section

--- src/app/(sections)/festivales/2025/layout.tsx
+++ src/app/(sections)/festivales/2025/layout.tsx
@@ -1,12 +1,4 @@
-import localFont from 'next/font/local'
 import { ParallaxBackground } from './components/ParallaxBackground'
-import './styles/2025.css'
-
-const superFortress = localFont({
-  src: './fonts/SuperFortress.woff2',
-  variable: '--font-superfortress',
-  display: 'swap',
-})
 
 export default function Festival2025Layout({
   children,
@@ -16,7 +8,7 @@ export default function Festival2025Layout({
   return (
     <>
       <ParallaxBackground />
-      <div className={superFortress.variable}>{children}</div>
+      {children}
     </>
   )
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,11 @@ import type { Metadata } from 'next'
 import { Josefin_Sans, Noto_Sans } from 'next/font/google'
 import siteData from '@/data/site.json'
 import '@/styles/globals.css'
+import '@/styles/sections/festivales/2025/styles.css'
 import { TopBarInfo } from '@/components/TopBarInfo'
 import { Background } from '@/components/Background'
+import localFont from 'next/font/local'
+import { cn } from '@/lib/utils'
 
 const SITE = siteData
 
@@ -15,6 +18,12 @@ const josefinSans = Josefin_Sans({
 const notoSans = Noto_Sans({
   variable: '--font-noto-sans',
   subsets: ['latin'],
+})
+
+const superFortress = localFont({
+  src: '../../public/fonts/sections/festivales/2025/SuperFortress.woff2',
+  variable: '--font-superfortress',
+  display: 'swap',
 })
 
 export const metadata: Metadata = {
@@ -53,7 +62,12 @@ export default function RootLayout({
   return (
     <html lang={SITE.lang}>
       <body
-        className={`${josefinSans.variable} ${notoSans.variable} bg-fm-white font-noto relative flex size-full min-h-[100dvh] flex-col antialiased`}>
+        className={cn(
+          'bg-fm-white font-noto relative flex size-full min-h-[100dvh] flex-col antialiased',
+          josefinSans.variable,
+          notoSans.variable,
+          superFortress.variable,
+        )}>
         <Background />
         {SITE.top_bar.active && <TopBarInfo />}
         {children}
