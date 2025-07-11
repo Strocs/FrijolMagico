7113b746713a91221977dba59c95326c6cf5cab1
Strocs <strocsdev@gmail.com>
2025-07-11 18:54:44 -0400

    style: enhance ApprovedArtistsScrollDownSignal component with improved layout and button functionality

--- src/app/(sections)/festivales/2025/[categories]/components/ApprovedArtistsScrollDownSignal.tsx
+++ src/app/(sections)/festivales/2025/[categories]/components/ApprovedArtistsScrollDownSignal.tsx
@@ -14,24 +14,42 @@ export const ApprovedArtistsScrollDownSignal = ({
     ref.current.hidden = false
   }, [ref])
 
+  const handleScrollDown = () => {
+    window.scrollTo({
+      top: document.body.scrollHeight,
+    })
+  }
+
   return (
     <div
-      ref={ref}
       hidden
-      className='fixed right-0 bottom-1/4 left-0 z-0 mx-auto flex w-fit flex-col items-center transition-opacity duration-500 not-landscape:bottom-4 2xl:bottom-1/3'>
-      <svg
-        xmlns='http://www.w3.org/2000/svg'
-        className='stroke-2025-white size-20 animate-bounce sm:size-24'
-        viewBox='0 0 24 24'
-        fill='none'
-        strokeWidth='3'
-        strokeLinecap='round'
-        strokeLinejoin='round'>
-        <path d='M12 5v14m7-7l-7 7-7-7' />
-      </svg>
-      <span className='font-superfortress text-stroke-3 text-stroke-2025-white text-2025-orange w-full max-w-[180px] text-center leading-none [paint-order:stroke_fill] sm:max-w-full sm:text-xl'>
-        Scroll para revelar a lxs artistas seleccionados
-      </span>
+      ref={ref}
+      className='font-superfortress pointer-events-none fixed right-0 bottom-1/4 left-0 z-50 mx-auto flex w-fit flex-col items-center gap-1 transition-opacity duration-500 2xl:bottom-1/3'>
+      <div>
+        <svg
+          xmlns='http://www.w3.org/2000/svg'
+          className='stroke-2025-yellow pointer-events-none mx-auto size-20 animate-bounce sm:size-24'
+          viewBox='0 0 24 24'
+          fill='none'
+          strokeWidth='3'
+          strokeLinecap='round'
+          strokeLinejoin='round'>
+          <path d='M12 5v14m7-7l-7 7-7-7' />
+        </svg>
+        <span className='text-stroke-3 text-stroke-2025-white text-2025-orange pointer-events-none w-full max-w-[180px] text-center text-lg leading-none [paint-order:stroke_fill] sm:max-w-full'>
+          Scroll para revelar
+        </span>
+        <span className='text-2025-white pointer-events-none block text-center'>
+          o
+        </span>
+      </div>
+
+      <button
+        type='button'
+        onClick={handleScrollDown}
+        className='bg-2025-orange text-2025-white hover:bg-2025-white hover:text-2025-orange pointer-events-auto cursor-pointer rounded-lg px-3 py-1 text-xl transition-colors'>
+        Ver todos ahora
+      </button>
     </div>
   )
 }
