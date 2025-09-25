0b909bd55c7fdbcaa73dacc8d7bc2e99db9c2b85
Strocs <strocsdev@gmail.com>
2025-09-25 10:53:47 -0300

    feat: render festival schedule on programacion page

--- src/app/(sections)/festivales/2025/programacion/page.tsx
+++ src/app/(sections)/festivales/2025/programacion/page.tsx
@@ -1,7 +1,19 @@
+import { Schedule } from './components/Schedule'
 import { getFestivalScheduleData } from './lib/getFestivalScheduleData'
 
-const { data, error } = await getFestivalScheduleData()
+const {
+  data: { firstDay, secondDay },
+  error,
+} = await getFestivalScheduleData()
 
 export default function ProgramacionPage() {
-  return <div></div>
+  if (error) {
+    return <div>Error: {error.message}</div>
+  }
+
+  return (
+    <div>
+      <Schedule schedule={firstDay} />
+    </div>
+  )
 }
