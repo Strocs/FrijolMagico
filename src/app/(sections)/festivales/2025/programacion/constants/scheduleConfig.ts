abdd61640c2fc099019092fc4b982332949e3af6
Strocs <strocsdev@gmail.com>
2025-09-25 13:42:45 -0300

    fix(schedule): update sheetIndex in scheduleConfig

--- src/app/(sections)/festivales/2025/programacion/constants/scheduleConfig.ts
+++ src/app/(sections)/festivales/2025/programacion/constants/scheduleConfig.ts
@@ -1,7 +1,7 @@
 import { CMSConfig } from '@/lib/getDataFromCMS'
 
 export const SCHEDULE_CONFIG: CMSConfig = {
-  sheetIndex: [2, 3],
+  sheetIndex: [1, 2],
   startRow: 3,
   sheetId: process.env.FESTIVAL_2025_SCHEDULE_SHEET_ID,
 }
