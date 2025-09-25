102bf8634999fb929f566a5c4b799c52a852a451
Strocs <strocsdev@gmail.com>
2025-09-24 23:43:36 -0300

    refactor(types): update and clean up global types

--- src/types/artists.d.ts
+++ src/types/artists.d.ts
@@ -8,26 +8,3 @@ export interface BaseArtist {
   category: Categories
   rrss: string
 }
-
-export interface RawCatalogArtist extends BaseArtist {
-  city: string
-  country: string
-  bio: string
-  email: string
-  avatar: string
-  collective: string
-}
-
-export interface Collective {
-  name: string
-  members: {
-    id: string
-    name: string
-  }[]
-}
-
-export interface CatalogArtist extends RawCatalogArtist {
-  collective: Collective | null
-}
-
-export type ApprovedArtist = BaseArtist
--- src/types/errors.d.ts
+++ src/types/errors.d.ts
@@ -1,3 +1,3 @@
-export interface ErrorObject {
+export type ErrorObject = {
   message: string
-}
+} | null
