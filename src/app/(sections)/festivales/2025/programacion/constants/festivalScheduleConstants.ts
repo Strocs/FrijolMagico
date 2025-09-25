d4bd1c122d8aed88dc6632a33b156b7fb3e831e5
Strocs <strocsdev@gmail.com>
2025-09-25 14:50:05 -0300

    feat(schedule): add workshop form selection mapping

--- src/app/(sections)/festivales/2025/programacion/constants/festivalScheduleConstants.ts
+++ src/app/(sections)/festivales/2025/programacion/constants/festivalScheduleConstants.ts
@@ -13,3 +13,26 @@ export const FESTIVAL_SCHEDULE_SHEET_HEADERS: ScheduleHeaders = {
 
 export const WORKSHOP_INSCRIPTION_LINK =
   'https://docs.google.com/forms/d/e/1FAIpQLSezrrJB5IyLY9ly85j573XdW_vHmt90zFEgvImDr3o4QpLe0A/viewform'
+
+// TODO: improve this (maybe fetch from a CMS or something) only if is necessary
+export const WORKSHOP_FORM_SELECTION = {
+  'Carlos Carvajal': 'Creación de comic - Carlos Carvajal',
+  'Isabela Adaos':
+    'Diseño y creación de personajes ilustración - Isabela Adaos',
+  'Camila Malebran':
+    'Aprende a dibujar escenas dinámicas para tus viñetas o videos a través de un storyboard - Camila Malebran',
+  'Rodan Castro':
+    'Métodos para saltarse el bloqueo creativo y terminar un comic - Rodan Castro',
+  'Diego Maya':
+    '¡IMPACTO! Hagamos una página de cómic llena de acción - Diego Maya',
+  'Javiera Ramirez':
+    'Crea y diseña tus propios personajes mecha/robot con la ayuda de formas geométricas! - Javiera Ramírez',
+  'Raquel Alarcon':
+    'Flora y Fauna territorial como inspiración para ilustrar - Raquel Alarcón',
+  'Marcela Trujillo (Maliki)': 'Taller de cómic autobiográfico - Maliki',
+  'Catalia Corvalan':
+    'Taller de diseño de personajes para cómic - Catalina Corvalan',
+  'Matias Palominos':
+    '"Reencarnatoys", reciclaje para crear obra - Matias Palominos',
+  'Gabriel Navarrete (Garvo)': 'De la idea a la publicación - Garvo',
+}
