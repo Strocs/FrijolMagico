8b9e6def81cb64e70d059368b86156391780241c
Strocs <strocsdev@gmail.com>
2025-09-25 13:43:01 -0300

    refactor(schedule): update scheduleMapper to new mapping logic and business rules

--- src/app/(sections)/festivales/2025/programacion/utils/scheduleMapper.ts
+++ src/app/(sections)/festivales/2025/programacion/utils/scheduleMapper.ts
@@ -1,18 +1,39 @@
 import { AppSchedule, RawSchedule, ScheduleHeaders } from '../types/schedule'
-import { FESTIVAL_SCHEDULE_SHEET_HEADERS } from '../constants/festivalScheduleConstants'
+import {
+  FESTIVAL_SCHEDULE_SHEET_HEADERS,
+  WORKSHOP_INSCRIPTION_LINK,
+} from '../constants/festivalScheduleConstants'
 
 export const mapToAppSchedule = (
-  rawItem: RawSchedule,
+  rawList: RawSchedule[],
   headers: ScheduleHeaders = FESTIVAL_SCHEDULE_SHEET_HEADERS,
-): AppSchedule => {
-  return {
-    startingTime: String(rawItem[headers.startingTime] ?? ''),
-    duration: Number(rawItem[headers.duration] ?? 0),
-    activityType: String(rawItem[headers.activityType] ?? ''),
-    title: String(rawItem[headers.title] ?? ''),
-    description: String(rawItem[headers.description] ?? ''),
-    speaker: String(rawItem[headers.speaker] ?? ''),
-    speakerSocialLink: String(rawItem[headers.speakerSocialLink]) ?? null,
-    infoLink: String(rawItem[headers.infoLink]) ?? null,
-  }
+): AppSchedule[] => {
+  return rawList
+    .map((item) => {
+      return {
+        startingTime: String(item[headers.startingTime] ?? ''),
+        duration: Number(item[headers.duration] ?? 0),
+        activityType: String(item[headers.activityType] ?? ''),
+        title: String(item[headers.title] ?? ''),
+        description: String(item[headers.description] ?? ''),
+        speaker: String(item[headers.speaker] ?? ''),
+        speakerSocialLink: item[headers.speakerSocialLink]
+          ? String(item[headers.speakerSocialLink])
+          : null,
+        infoLink: !!item[headers.infoLink]
+          ? String(item[headers.infoLink])
+          : null,
+        inscriptionLink:
+          // This is business rule
+          item[headers.activityType]?.toLowerCase().trim() === 'taller'
+            ? WORKSHOP_INSCRIPTION_LINK
+            : null,
+      }
+    })
+    .filter(
+      (item) =>
+        // this is business rules
+        item.activityType.toLowerCase().trim() !== 'cierre' &&
+        item.activityType.toLowerCase().trim() !== 'ventana',
+    )
 }
