4b7e022d7afd54117445678388455b02d1b408e8
Strocs <strocsdev@gmail.com>
2025-09-25 10:53:39 -0300

    refactor: update schedule types and rendering logic

--- src/app/(sections)/festivales/2025/programacion/components/Schedule.tsx
+++ src/app/(sections)/festivales/2025/programacion/components/Schedule.tsx
@@ -1,41 +1,47 @@
 import { cn } from '@/utils/utils'
 import { ScheduleItem } from './ScheduleItem'
-import { type ScheduleItem as ScheduleItemProps } from '../types/schedule'
+import { type AppSchedule } from '../types/schedule'
 
 interface ScheduleProps {
-  schedule: Array<ScheduleItemProps>
+  schedule: Array<AppSchedule>
 }
 
 export const Schedule = ({ schedule }: ScheduleProps) => {
-  // get time list.
-  //
-  const time = [
-    '10:00',
-    '12:00',
-    '14:00',
-    '16:00',
-    '18:00',
-    '20:00',
-    '22:00',
-    '24:00',
-  ]
+  const getTimeList = (day: AppSchedule[]) => {
+    const times = day.map((item) => item.startingTime)
+    return Array.from(new Set(times))
+  }
+
+  const getEventByTime = (time: string, events: AppSchedule[]) => {
+    return events.filter((event) => event.startingTime === time)
+  }
+
+  const timeList = getTimeList(schedule)
 
   return (
-    <div className='space-y-10'>
-      <hr className={cn(['border-2 border-dashed'])} />
-      <section className='flex h-fit gap-2 md:gap-10'>
-        <h3
-          className={cn([
-            'font-fira -scale-100 text-end text-2xl leading-[1.5rem] font-black tracking-wider [writing-mode:vertical-lr] md:text-4xl',
-          ])}>
-          {time[0]}
-        </h3>
-        <ul className='flex w-full flex-wrap gap-x-6 gap-y-10'>
-          {schedule.map((activity) => (
-            <ScheduleItem key={activity.title} {...activity} />
-          ))}
-        </ul>
-      </section>
-    </div>
+    <section className='grid gap-24'>
+      {timeList.map((time, index) => {
+        const eventsbyTime = getEventByTime(time, schedule)
+
+        return (
+          <div key={time + index} className='space-y-10'>
+            <hr className={cn(['border-2 border-dashed'])} />
+            <section className='flex h-fit gap-2 md:gap-10'>
+              <h3
+                className={cn([
+                  'font-fira -scale-100 text-end text-2xl leading-[1.5rem] font-black tracking-wider [writing-mode:vertical-lr] md:text-4xl',
+                ])}>
+                {time}
+              </h3>
+              <ul className='flex w-full flex-wrap gap-x-6 gap-y-10'>
+                {eventsbyTime.map((event) => (
+                  <ScheduleItem key={event.title} {...event} />
+                ))}
+              </ul>
+            </section>
+          </div>
+        )
+      })}
+    </section>
   )
 }
--- src/app/(sections)/festivales/2025/programacion/components/ScheduleItem.tsx
+++ src/app/(sections)/festivales/2025/programacion/components/ScheduleItem.tsx
@@ -1,45 +1,53 @@
 import { cn } from '@/utils/utils'
-import { type ScheduleItem as ScheduleItemProps } from '../types/schedule'
+import { type AppSchedule as ScheduleItemProps } from '../types/schedule'
 import { ScheduleItemLabel } from './SchceduleItemLabel'
 import { ScheduleItemButton } from './ScheduleItemButton'
+import { Instagram } from '@/components/icons/Instagram'
 
 export const ScheduleItem = ({
   title,
   startingTime,
   duration,
   activityType,
-  inscriptionUrl,
-  emissor,
+  infoLink,
+  speaker,
+  speakerSocialLink,
 }: ScheduleItemProps) => {
   return (
     <li
       className={cn([
-        'hover:text-secondary group relative grid h-fit min-h-44 w-full max-w-[340px] gap-2 px-4 pt-8 leading-4 text-black shadow-xl duration-300',
-        "before:bg-secondary before:absolute before:-z-20 before:h-full before:w-full before:content-['']",
-        "group after:absolute after:bottom-0 after:-z-10 after:h-7 after:w-full after:duration-300 after:content-[''] hover:after:h-full",
+        'hover:text-2025-orange text-fm-black before:bg-2025-white',
+        'group relative grid h-fit min-h-44 w-full max-w-[340px] gap-2 px-4 pt-8 leading-4 shadow-lg duration-300',
+        "before:absolute before:-z-20 before:h-full before:w-full before:content-['']",
+        "after:absolute after:bottom-0 after:-z-10 after:h-7 after:w-full after:duration-300 after:content-[''] hover:after:h-full",
       ])}>
       <ScheduleItemLabel>{activityType}</ScheduleItemLabel>
       <header>
-        <h2 className='group-hover:text-secondary font-bold tracking-wide text-black uppercase duration-300'>
+        <h2 className='group-hover:text-2025-orange text-fm-black font-bold tracking-wide uppercase duration-300'>
           {title}
         </h2>
-        <span className='text-sm leading-3 font-light'>{emissor}</span>
+        <p className='text-sm leading-3 font-light'>
+          {speaker}{' '}
+          <a aria-label='Redes sociales del encargado' href={speakerSocialLink}>
+            <Instagram size={10} />
+          </a>
+        </p>
       </header>
       <section className='flex items-end justify-between gap-4 self-end'>
         <div>{startingTime}</div>
       </section>
-      <footer className='flex h-fit w-full items-center justify-center gap-2 self-end text-center text-white'>
+      <footer className='text-fm-white flex h-fit w-full items-center justify-center gap-2 self-end text-center'>
         <ScheduleItemButton
           as='a'
-          className='hover:text-accent-yellow w-full py-1.5 whitespace-nowrap transition duration-300'>
+          className='hover:text-2025-yellow w-full py-1.5 whitespace-nowrap transition duration-300'>
           Más info
         </ScheduleItemButton>
-        {!!inscriptionUrl ? (
+        {!!infoLink ? (
           <>
             <hr className='border-secondary w-4 rotate-90 border-1' />
             <a
               className='hover:text-accent-yellow block w-full py-1.5 transition duration-300'
-              href={inscriptionUrl}>
+              href={infoLink}>
               Inscríbete aquí
             </a>
           </>
