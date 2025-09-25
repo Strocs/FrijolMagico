9cb5a061a46d346f846318804bcb7870508fa908
Strocs <strocsdev@gmail.com>
2025-09-25 15:23:52 -0300

    refactor(festival2025): remove redirect and add placeholder content

--- src/app/(sections)/festivales/2025/page.tsx
+++ src/app/(sections)/festivales/2025/page.tsx
@@ -1,6 +1,3 @@
-import { redirect } from 'next/navigation'
-import { paths } from '@/config/paths'
-
 export default function Festival2025Page() {
-  redirect(paths.festival[2025].base + '/ilustracion')
+  return <div>2025</div>
 }
