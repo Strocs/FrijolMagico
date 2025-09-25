29cc25f70c9c8fd3be9c4cc95518ab8b1329499b
Strocs <strocsdev@gmail.com>
2025-09-25 10:53:36 -0300

    style: update schedule item label colors and capitalization

--- src/app/(sections)/festivales/2025/programacion/components/SchceduleItemLabel.tsx
+++ src/app/(sections)/festivales/2025/programacion/components/SchceduleItemLabel.tsx
@@ -4,7 +4,7 @@ interface ScheduleItemLabelProps {
 
 export const ScheduleItemLabel = ({ children }: ScheduleItemLabelProps) => {
   return (
-    <span className='absolute -top-3 -skew-y-6 bg-black px-4 py-2 font-bold text-white uppercase'>
+    <span className='bg-fm-black text-2025-white absolute -top-3 -skew-y-6 px-4 py-2 font-bold capitalize'>
       {children}
     </span>
   )
