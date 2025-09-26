28eda76fb016d14232582875ad399e94b12e631c
Strocs <strocsdev@gmail.com>
2025-09-26 12:50:21 -0300

    refactor(programacion): update ScheduleItemLabel API and color logic

--- src/app/(sections)/festivales/2025/programacion/components/SchceduleItemLabel.tsx
+++ src/app/(sections)/festivales/2025/programacion/components/SchceduleItemLabel.tsx
@@ -1,11 +1,19 @@
+import { cn } from '@/utils/utils'
+
 interface ScheduleItemLabelProps {
-  children: React.ReactNode
+  text: string
 }
 
-export const ScheduleItemLabel = ({ children }: ScheduleItemLabelProps) => {
+export const ScheduleItemLabel = ({ text }: ScheduleItemLabelProps) => {
+  const labelColor =
+    text.toLowerCase().trim() === 'taller' ? 'bg-2025-purple' : 'bg-fm-black'
   return (
-    <span className='bg-fm-black text-2025-white absolute -top-3 -skew-y-6 px-4 py-2 font-bold capitalize'>
-      {children}
+    <span
+      className={cn(
+        'bg-fm-black text-2025-white absolute -top-3 -skew-y-6 px-4 py-2 font-bold capitalize',
+        labelColor,
+      )}>
+      {text}
     </span>
   )
 }
