0ea2d3921a9b31a60f040eedd01036d3d6bfd431
Strocs <strocsdev@gmail.com>
2025-09-26 12:50:36 -0300

    feat(programacion): parse multiple speakers and social links in scheduleMapper

--- src/app/(sections)/festivales/2025/programacion/utils/scheduleMapper.ts
+++ src/app/(sections)/festivales/2025/programacion/utils/scheduleMapper.ts
@@ -1,7 +1,6 @@
 import { AppSchedule, RawSchedule, ScheduleHeaders } from '../types/schedule'
 import {
   FESTIVAL_SCHEDULE_SHEET_HEADERS,
-  WORKSHOP_FORM_SELECTION,
   WORKSHOP_INSCRIPTION_LINK,
 } from '../constants/festivalScheduleConstants'
 
@@ -9,36 +8,69 @@ export const mapToAppSchedule = (
   rawList: RawSchedule[],
   headers: ScheduleHeaders = FESTIVAL_SCHEDULE_SHEET_HEADERS,
 ): AppSchedule[] => {
-  return rawList
-    .map((item) => {
-      return {
-        startingTime: String(item[headers.startingTime] ?? ''),
-        duration: Number(item[headers.duration] ?? 0),
-        activityType: String(item[headers.activityType] ?? ''),
-        title: String(item[headers.title] ?? ''),
-        description: String(item[headers.description] ?? ''),
-        speaker: String(item[headers.speaker] ?? ''),
-        speakerSocialLink: item[headers.speakerSocialLink]
-          ? String(item[headers.speakerSocialLink])
-          : null,
-        infoLink: !!item[headers.infoLink]
-          ? String(item[headers.infoLink])
-          : null,
-        inscriptionLink:
-          // This is business rule
-          item[headers.activityType]?.toLowerCase().trim() === 'taller'
-            ? WORKSHOP_INSCRIPTION_LINK +
-                '?entry.2092970081=' +
-                WORKSHOP_FORM_SELECTION[
-                  item[headers.speaker] as keyof typeof WORKSHOP_FORM_SELECTION
-                ] || ''
-            : null,
-      }
-    })
-    .filter(
-      (item) =>
-        // this is business rules
-        item.activityType.toLowerCase().trim() !== 'cierre' &&
-        item.activityType.toLowerCase().trim() !== 'ventana',
-    )
+  const appSchedule = rawList.map((item) => {
+    return {
+      startingTime: String(item[headers.startingTime] ?? ''),
+      duration: Number(item[headers.duration] ?? 0),
+      activityType: String(item[headers.activityType] ?? ''),
+      title: String(item[headers.title] ?? ''),
+      description: String(item[headers.description] ?? ''),
+      speaker: parseSpeaker(String(item[headers.speaker])),
+      speakerSocialLink: parseSpeakerSocialLink(
+        String(item[headers.speakerSocialLink]),
+      ),
+      infoLink: !!item[headers.infoLink]
+        ? String(item[headers.infoLink])
+        : null,
+      inscriptionLink: WORKSHOP_INSCRIPTION_LINK,
+    }
+  })
+
+  return filterUnrelatedActivities(appSchedule)
+}
+
+const parseSpeaker = (
+  speaker: RawSchedule['Encargado'],
+): AppSchedule['speaker'] => {
+  if (!speaker || speaker.trim() === '') return ''
+
+  const speakersList = speaker.split(',').map((s) => s.trim())
+
+  if (speakersList.length > 1) {
+    return speakersList
+  }
+
+  if (speakersList.length === 1) {
+    return speakersList[0]
+  }
+
+  return ''
+}
+
+const parseSpeakerSocialLink = (
+  speakerSocialLink: RawSchedule['RRSS'],
+): AppSchedule['speakerSocialLink'] => {
+  if (!speakerSocialLink || speakerSocialLink.trim() === '') return null
+
+  const speakerSocialLinkList = speakerSocialLink
+    .split(',')
+    .map((s) => s.trim())
+
+  if (speakerSocialLinkList.length > 1) {
+    return speakerSocialLinkList
+  }
+
+  if (speakerSocialLinkList.length === 1) {
+    return speakerSocialLinkList[0]
+  }
+
+  return null
+}
+
+const filterUnrelatedActivities = (schedule: AppSchedule[]): AppSchedule[] => {
+  return schedule.filter(
+    (item) =>
+      item.activityType.toLowerCase().trim() !== 'cierre' &&
+      item.activityType.toLowerCase().trim() !== 'ventana',
+  )
 }
